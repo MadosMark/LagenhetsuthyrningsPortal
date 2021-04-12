d424c1bd987e2293ffcaca6a57d6bbfc65a57dde
MadosMark <69796358+MadosMark@users.noreply.github.com>
2021-04-12 15:17:37 +0200

    Publish function - Done

--- app/Http/Controllers/PublishController.php
+++ app/Http/Controllers/PublishController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Apartment;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class PublishController extends Controller
 {
@@ -15,12 +18,35 @@ class PublishController extends Controller
      */
 
 
-    public function __invoke()
+    public function __invoke(Request $request)
     {
         $user = Auth::user();
 
-        return view('publish', [
-            'user' => $user
+
+        $this->validate(request(), [
+            'address' => ['required', 'string'],
+            'city' => ['required', 'string'],
+            'rent' => ['required', 'integer'],
+            'rooms' => ['required', 'integer'],
+            'description' => ['required', 'string'],
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
+
+        $imageName = time() . '.' . $request->image->extension();
+        $request->image->move(public_path('images'), $imageName);
+
+
+        $address = $request->input('address');
+        $city = $request->input('city');
+        $rent = $request->input('rent');
+        $rooms = $request->input('rooms');
+        $description = $request->input('description');
+
+        $data = array('user_id' => $user->id, 'address' => $address, "city" => $city, "rent" => $rent, "rooms" => $rooms, "description" => $description, 'image' => $imageName);
+        Apartment::insert($data);
+
+
+
+        return redirect('/dashboard');
     }
 }
--- resources/views/publish.blade.php
+++ resources/views/publish.blade.php
@@ -4,51 +4,37 @@
     <div class="publish_body">
     <section class="publish_container">
     <h1 class="publish_title">Fyll i nedan för annonsering:</h1>
-    <form class="row g-3">
 
-        <div class="col-12">
-          <label for="inputAddress" class="form-label">Adress</label>
-          <input type="text" class="form-control" id="inputAddress" placeholder="">
-        </div>
-        <div class="col-md-6">
-          <label for="inputCity" class="form-label">Stad</label>
-          <input type="text" class="form-control" id="inputCity">
-        </div>
+    <form method="POST" action="/publish" enctype="multipart/form-data">
+        @csrf
+          <label for="address" class="form-label">Adress</label>
+          <input name="address" type="address" class="form-control" id="inputAddress" placeholder="">
+          <label for="city" class="form-label">Stad</label>
+          <input name="city" type="city" class="form-control" id="inputCity">
+          <label for="rent" class="form-label">Hyra</label>
+          <input name="rent" type="rent" class="form-control" id="inputAddress" placeholder="">
+          <label for="rooms" class="form-label">Rum</label>
+          <select name="rooms" id="rooms" class="form-select" aria-label="Default select example">
+          <option selected>Antal rum</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          </select>
+          <label for="image" class="form-label">Ladda upp bild här:</label>
+            <input class="form-control" type="file" id="image" name="image">
+          <label for="description">Beskrivning</label>
+          <textarea name="description" class="form-control" placeholder="Beskrivning" id="description" style="height: 100px"></textarea>
 
-        <div class="col-md-2">
-          <label for="inputZip" class="form-label">Postkod</label>
-          <input type="text" class="form-control" id="inputZip">
-        </div>
-        <div class="col-12">
-            <label for="inputAddress" class="form-label">Hyra</label>
-            <input type="text" class="form-control" id="inputAddress" placeholder="">
-          </div>
+          <button type="submit" class="btn btn-primary">Ladda upp</button>
+        </form>
 
-
-        <div class="mb_3">
-            <label for="formFile" class="form-label">Ladda upp bild här:</label>
-            <input class="form-control" type="file" id="formFile">
-        </div>
-        <div>
-            <select class="form-select" aria-label="Default select example">
-            <option selected>Antal rum</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            </select>
-        </div>
-        <div class="form-floating">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-            <label for="floatingTextarea2">Beskrivning</label>
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary">Ladda upp</button>
+        <div class="error">
+            @include('errors')
             </div>
-        </div>
-      </form>
 
 
 
